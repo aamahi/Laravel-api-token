6d9df51b298719ea9c92716e61a5a5dfdd2ccf99
Mahi <amahi.wp@gmail.com>
2020-05-27 01:15:11 +0600

    Create dumy user using forceCreated

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::get('/artical',[ArticalController::class],'getAllAritcals');
+Route::get('/artical/{id}',[ArticalController::class],'getAritcals');
+Route::post('/artical',[ArticalController::class],'createAritcal');
+Route::put('/artical/{id}',[ArticalController::class],'updateAritcal');
+Route::delete('/artical/{id}',[ArticalController::class],'deleteAritcal');
+
+
+Route::get('/create',function (){
+    \App\User::forceCreate([
+        'name'=>"Fathem Jannat Rapa",
+        'email'=>"rapa@mahi.com",
+        'password'=>Hash::make('rapamahi'),
+    ]);
+    \App\User::forceCreate([
+        'name'=>"Abdullah Al Mahi",
+        'email'=>"mahi@rapa.com",
+        'password'=>Hash::make('rapamahi'),
+    ]);
 });
